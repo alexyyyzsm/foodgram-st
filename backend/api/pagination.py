f38695ce9766e0e4a92dd78f05490827a6b1b046
alexyyyzsm <163728328+alexyyyzsm@users.noreply.github.com>
2025-06-10 00:01:25 +0300

    Delete backend directory

--- backend/api/pagination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-from constants import PAGE_SIZE
-
-
-class CustomPagePagination(PageNumberPagination):
-    page_size_query_param = 'limit'
-    page_size = PAGE_SIZE
